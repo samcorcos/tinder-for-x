45b177fce098615bd425819ec0d10f0334c71ea0
Sam Corcos <samcorcos@gmail.com>
2015-06-22 09:59:29 +0300

    initial commit

---
+++ client/views/home.jsx
@@ -0,0 +1,16 @@
+
+// var MyComponent = React.createClass({
+//   mixins: [ReactMeteor.Mixin],
+//
+//   startMeteorSubscriptions: function() {
+//     Meteor.subscribe("players");
+//   },
+//
+//   // Make sure your component implements this method.
+//   getMeteorState: function() {
+//     return {
+//       playerCount: Players.find().count(),
+//       ...
+//     };
+//   }
+// });
